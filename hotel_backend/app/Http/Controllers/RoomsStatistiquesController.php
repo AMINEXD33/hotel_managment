afde6e8b8f5f55685e34ee6d028a2fcd9ea53d7e
AMINEXD33 <amine.gamer222@gmail.com>
2024-12-04 20:43:49 +0100

    created models, seeders based on the planed db (empty)

---
+++ hotel_backend/app/Http/Controllers/RoomsStatistiquesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Storerooms_statistiquesRequest;
+use App\Http\Requests\Updaterooms_statistiquesRequest;
+use App\Models\rooms_statistiques;
+
+class RoomsStatistiquesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storerooms_statistiquesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(rooms_statistiques $rooms_statistiques)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(rooms_statistiques $rooms_statistiques)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updaterooms_statistiquesRequest $request, rooms_statistiques $rooms_statistiques)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(rooms_statistiques $rooms_statistiques)
+    {
+        //
+    }
+}
---
+++ hotel_backend/database/seeders/RoomsStatistiquesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoomsStatistiquesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
